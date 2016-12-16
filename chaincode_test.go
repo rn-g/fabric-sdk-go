e97cdd4dfe83124196754e05414441bafeba1e42
fqutishat <firas.qutishat@securekey.com>
2016-12-16 11:06:57 -0500

    Fix test case

--- chaincode_test.go
+++ chaincode_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/hyperledger/fabric/core/util"
 	pb "github.com/hyperledger/fabric/protos/peer"
@@ -24,6 +26,9 @@ func TestChainCodeInvoke(t *testing.T) {
 	chain.Orderer = CreateNewOrderer(fmt.Sprintf("%s:%s", config.GetOrdererHost(), config.GetOrdererPort()))
 	user := chain.GetMember("admin")
 	user.SetEnrollment(privateKey, publicKey)
+	value := getQueryValue(t, user)
+	fmt.Printf("*** QueryValue before invoke %s\n", value)
+
 	var endorsers []*Peer
 	for _, peer := range config.GetPeersConfig() {
 		endorsers = append(endorsers, CreateNewPeer(fmt.Sprintf("%s:%s", peer.Host, peer.Port)))
@@ -32,7 +37,7 @@ func TestChainCodeInvoke(t *testing.T) {
 	args = append(args, "move")
 	args = append(args, "a")
 	args = append(args, "b")
-	args = append(args, "100")
+	args = append(args, "1")
 	transactionProposalRequest := TransactionProposalRequest{Targets: endorsers, ChaincodeId: "mycc2", FunctionName: "invoke", Args: args,
 		ChainId: "**TEST_CHAINID**", TxId: util.GenerateUUID()}
 	transactionProposalResponse, proposal, err := user.SendTransactionProposal(transactionProposalRequest)
@@ -52,44 +57,44 @@ func TestChainCodeInvoke(t *testing.T) {
 	if err != nil {
 		t.Errorf("SendTransaction return error: %v", err)
 	}
+	fmt.Println("need to wait now for the committer to catch up")
+	time.Sleep(time.Second * 30)
+	valueAfterInvoke := getQueryValue(t, user)
+	fmt.Printf("*** QueryValue after invoke %s\n", valueAfterInvoke)
+
+	valueInt, _ := strconv.Atoi(value)
+	valueInt = valueInt + 1
+	valueAfterInvokeInt, _ := strconv.Atoi(valueAfterInvoke)
+	if valueInt != valueAfterInvokeInt {
+		t.Errorf("SendTransaction didn't change the QueryValue")
+	}
 
 }
 
-//func TestChainCodeQuery(t *testing.T) {
-//	privateKey, err := loadEnrollmentPrivateKey()
-//	if err != nil {
-//		t.Errorf("loadEnrollmentPrivateKey return error: %v", err)
-//	}
-//	publicKey, err := loadEnrollmentPublicKey()
-//	if err != nil {
-//		t.Errorf("loadEnrollmentPublicKey return error: %v", err)
-//	}
-//	chain := CreateNewChain("testchain")
-//	user := chain.GetMember("admin")
-//	user.SetEnrollment(privateKey, publicKey)
-//	var endorsers []*Peer
-//	for _, peer := range config.GetPeersConfig() {
-//		endorsers = append(endorsers, CreateNewPeer(fmt.Sprintf("%s:%s", peer.Host, peer.Port)))
+func getQueryValue(t *testing.T, user *Member) string {
+	var endorsers []*Peer
+	for _, peer := range config.GetPeersConfig() {
+		endorsers = append(endorsers, CreateNewPeer(fmt.Sprintf("%s:%s", peer.Host, peer.Port)))
+		break
+	}
+	var args []string
+	args = append(args, "query")
+	args = append(args, "b")
+	transactionProposalRequest := TransactionProposalRequest{Targets: endorsers, ChaincodeId: "mycc2", FunctionName: "invoke", Args: args,
+		ChainId: "**TEST_CHAINID**", TxId: util.GenerateUUID()}
+	transactionProposalResponse, _, err := user.SendTransactionProposal(transactionProposalRequest)
+	if err != nil {
+		t.Errorf("SendTransactionProposal return error: %v", err)
+	}
 
-//	}
-//	var args []string
-//	args = append(args, "query")
-//	args = append(args, "b")
-//	transactionProposalRequest := TransactionProposalRequest{Targets: endorsers, ChaincodeId: "mycc2", FunctionName: "invoke", Args: args,
-//		ChainId: "**TEST_CHAINID**", TxId: util.GenerateUUID()}
-//	transactionProposalResponse, _, err := user.SendTransactionProposal(transactionProposalRequest)
-//	if err != nil {
-//		t.Errorf("SendTransactionProposal return error: %v", err)
-//	}
-
-//	for _, v := range transactionProposalResponse {
-//		if v.Err != nil {
-//			t.Errorf("Endorser %s return error: %v", v.Endorser, v.Err)
-//		}
-//		fmt.Printf("Endorser '%s' return Query result: %v\n", v.Endorser, v.ProposalResponse.GetResponse())
-//	}
-
-//}
+	for _, v := range transactionProposalResponse {
+		if v.Err != nil {
+			t.Errorf("Endorser %s return error: %v", v.Endorser, v.Err)
+		}
+		return string(v.ProposalResponse.GetResponse().Payload)
+	}
+	return ""
+}
 
 func loadEnrollmentPrivateKey() ([]byte, error) {
 	raw, err := ioutil.ReadFile("./test_resources/private.pem")
