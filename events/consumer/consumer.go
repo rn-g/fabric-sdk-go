0a3f1d613dcc592fc7352248ca18d3ff732aff19
Divyank Katira <divyank.katira@securekey.com>
2017-02-10 11:26:32 -0500

    Updated license header on consumer.go
    Change-Id: I6d8eb799f60c6590d0174457c58bd634c36be551

--- events/consumer/consumer.go
+++ events/consumer/consumer.go
@@ -1,6 +1,5 @@
 /*
-Copyright SecureKey Technologies Inc. All Rights Reserved.
-
+Copyright IBM Corp, SecureKey Technologies Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
